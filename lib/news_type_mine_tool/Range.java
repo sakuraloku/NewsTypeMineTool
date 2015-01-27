c76cfc8334582b2d92c09963b971dfe7b2f690f2
sakuraloku <sakuraloku@users.noreply.github.com>
2015-01-27 17:46:48 +0800

    Create Range.java

---
+++ lib/news_type_mine_tool/Range.java
@@ -0,0 +1,23 @@
+package news_type_mine_tool;
+
+public class Range {
+	int startIndex/*inclusive*/ , endIndex/*exclusive*/;
+
+	public int getStartIndex() {
+		return startIndex;
+	}
+
+	public void setStartIndex(int startIndex) {
+		this.startIndex = startIndex;
+	}
+
+	public int getEndIndex() {
+		return endIndex;
+	}
+
+	public void setEndIndex(int endIndex) {
+		this.endIndex = endIndex;
+	}
+	
+	
+}
